5bd4f6ef88c6f66132f9c14150ab6a88e61c1450
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-06-08 16:51:10 +0300

    Updates

---
+++ precache-manifest.a3985376927cc52b82d44f87092bb0ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39930cc3201a78e21bfc",
+    "url": "/metre-react/static/js/runtime~main.af504eae.js"
+  },
+  {
+    "revision": "7f6a2951928c63fcfc5f",
+    "url": "/metre-react/static/js/main.8120f2a8.chunk.js"
+  },
+  {
+    "revision": "bfbb95492a8c6e6ba48d",
+    "url": "/metre-react/static/js/2.ac32eea5.chunk.js"
+  },
+  {
+    "revision": "7f6a2951928c63fcfc5f",
+    "url": "/metre-react/static/css/main.da307553.chunk.css"
+  },
+  {
+    "revision": "bfbb95492a8c6e6ba48d",
+    "url": "/metre-react/static/css/2.4aa8f26c.chunk.css"
+  },
+  {
+    "revision": "51c373a1069085b311dfc816bdf6a046",
+    "url": "/metre-react/index.html"
+  }
+];
