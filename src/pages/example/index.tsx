4b61b8841f45e7a59b866a7ad0b0d0e92dd33a8b
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-06-08 17:46:03 +0300

    little change for try cloud redeploy

--- src/pages/example/index.tsx
+++ src/pages/example/index.tsx
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
-import withTracker from '../../simulate';
+import withTracker from "../../simulate";
 
 export const SomePageComponent: any = ({
-  currentUser, setCurrentUser,
-  count, setCount,
+  currentUser,
+  setCurrentUser,
+  count,
+  setCount
 }) => {
-  return <>
-    <button onClick={() => setCurrentUser(currentUser ? 0 : 123)}>currentUser: {currentUser}</button>
-    <button onClick={() => setCount(count + 1)}>{count}</button>
-  </>;
-}
+  return (
+    <>
+      <button onClick={() => setCurrentUser(currentUser ? 0 : 123)}>
+        currentUser: {currentUser}
+      </button>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </>
+  );
+};
 
 export default withTracker(() => {
-  const [currentUser, setCurrentUser] = useState(123);
+  const [currentUser, setCurrentUser] = useState(0);
 
   return {
     currentUser: currentUser,
-    setCurrentUser,
+    setCurrentUser
   };
 })(({ currentUser, setCurrentUser }) => {
   const [count, setCount] = useState(0);
 
-  return <SomePageComponent currentUser={currentUser} setCurrentUser={setCurrentUser} count={count} setCount={setCount}/>;
-})
+  return (
+    <SomePageComponent
+      currentUser={currentUser}
+      setCurrentUser={setCurrentUser}
+      count={count}
+      setCount={setCount}
+    />
+  );
+});
