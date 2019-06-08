853cdb9fde512aeb6c0410cfc85989631bd91527
Travis CI User <travis@example.org>
2019-06-08 16:18:12 +0000

    Updates

---
+++ precache-manifest.9553de28eec9cb9c9f7120e860a7337d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260cd018ac9a030d7b32c8f9e517b3ed",
+    "url": "/metre-react/index.html"
+  },
+  {
+    "revision": "3194554bc08e83364c3c",
+    "url": "/metre-react/static/css/2.23cf21a5.chunk.css"
+  },
+  {
+    "revision": "35ab314e11cd60d61b1d",
+    "url": "/metre-react/static/css/main.c6b16ca8.chunk.css"
+  },
+  {
+    "revision": "3194554bc08e83364c3c",
+    "url": "/metre-react/static/js/2.a9bc1643.chunk.js"
+  },
+  {
+    "revision": "35ab314e11cd60d61b1d",
+    "url": "/metre-react/static/js/main.a8946eca.chunk.js"
+  },
+  {
+    "revision": "39930cc3201a78e21bfc",
+    "url": "/metre-react/static/js/runtime~main.af504eae.js"
+  }
+]);
